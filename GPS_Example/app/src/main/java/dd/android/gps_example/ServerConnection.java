207830e7bd4b02b0551520fe1fea91198bd3664f
Deregi Diána <deregidia@gmail.com>
2016-10-22 17:02:28 +0300

    Upload project

---
+++ GPS_Example/app/src/main/java/dd/android/gps_example/ServerConnection.java
@@ -0,0 +1,13 @@
+package dd.android.gps_example;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DoubleD on 2016. 10. 21..
+ */
+
+public class ServerConnection {
+
+    /* Ask the server for interesting places */
+
+}
